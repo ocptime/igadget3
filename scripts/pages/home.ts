9f83ef0fdd5346b5150044b338989a931d960b93
Sam George <sgcet@sams-mbp.home>
2014-03-04 18:09:01 -0500

    Home page finish

--- scripts/pages/home.ts
+++ scripts/pages/home.ts
@@ -1,10 +1,35 @@
 # Place holder/example file
 
 $("./body") {
-	add_class("_home")
+	add_class("mw_home")
 
+	$(".//div") {
+		remove_class("QuickView")
+	}
 
+	$(".//div[@id='HomeFeaturedProducts' or @id='HomeNewProducts']") {
+		$("./h2") {
+			add_class("_page-title")
+		}
+		$("./div[@class='BlockContent']/ul") {
+			// change the scopped ul to div
+			name("div")
+			$("./li") {
+				// li to div
+				name("div")
+				attribute("class", "_item-wrap")
+				attribute("style","")
+				$("./div[@class='ProductActionAdd']") {
+					$('./a') {
+						inner_wrap("div", class: "_cart-button")
+					}
+				}
+			}
+		}
+	}
 
-
-	
 }
+
+
+
+
