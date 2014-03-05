e9578262b779d7790974cccd90f22d62c1d350e2
Sam George <sgcet@sams-mbp.home>
2014-03-05 13:00:19 -0500

    Shop page started

--- scripts/mappings.ts
+++ scripts/mappings.ts
@@ -22,6 +22,10 @@ match($status) {
         log("--> Importing pages/home.ts in mappings.ts")
         @import pages/home.ts
       }
+      with(/shop-/) {
+        log("--> Importing pages/category.ts in mappings.ts")
+        @import pages/category.ts
+      }
       else() {
         log("--> No page match in mappings.ts")
       }
